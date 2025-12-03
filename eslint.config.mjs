b41f509e9b51d409543a4b081e20bb58a0b7b4a5
Jay Kadlag <jaykadlag14@gmail.com>
2025-12-03 16:06:53 +0530

    feat: setup pre-commit hook with lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,8 @@ import {defineConfig, globalIgnores} from "eslint/config";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  prettier,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
   {
     plugins: {
       "check-file": checkFile,
@@ -37,7 +31,7 @@ const eslintConfig = defineConfig([
       "check-file/folder-naming-convention": [
         "error",
         {
-          "src/**/": "KEBAB_CASE",
+          "src/**/!(*)*": "KEBAB_CASE",
         },
       ],
     },
